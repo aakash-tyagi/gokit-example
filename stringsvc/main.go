8cc3d732578f4917c107995cdcd372ad05e035e7
aakash-tyagi <65077073+aakash-tyagi@users.noreply.github.com>
2024-08-29 22:56:52 +0530

    string service

--- stringsvc/main.go
+++ stringsvc/main.go
@@ -1,7 +1,120 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+var ErrEmpty = errors.New("empty string")
+
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+type UppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type UppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"`
+}
+
+type CountRequest struct {
+	S string `json:"s"`
+}
+
+type CountResponse struct {
+	V int `json:"v"`
+}
+
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(UppercaseRequest)
+
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return UppercaseResponse{v, err.Error()}, nil
+		}
+
+		return UppercaseResponse{v, ""}, nil
+	}
+
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(CountRequest)
+
+		v := svc.Count(req.S)
+
+		return CountResponse{v}, nil
+	}
+}
 
 func main() {
-	fmt.Println("test git push")
+
+	svc := stringService{}
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request UppercaseRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+
+	return request, nil
+}
+
+func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request CountRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
 }
